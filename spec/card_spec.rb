cd7c57031a7dd37a8aedd9ac7b03ffc2ccfcaba8
MMcClure11 <mcclme02@luther.edu>
2021-03-12 09:38:09 -0600

    Refactor before hook to use helper method card, allows us to not use an instance variable

--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -9,17 +9,20 @@ end
 
 RSpec.describe Card do
   #refactor, using the actual Class reference is better than the string because it adds some advanced RSpec helper methods 
-  before do 
-    @card = Card.new('Ace', 'Spades')
-  end
-  #this will be run once before each test
+  # before do 
+  #   @card = Card.new('Ace', 'Spades')
+  # end
+  #before hook - this will be run once before each test
   #note instance variables are often not preferred because if an instance variable is undefined, you won't get an error it will evaluate to nil which can bake it more difficult to debug
-  
+  def card 
+    Card.new('Ace', 'Spades')
+  end
+
   it 'has a rank' do 
-    expect(@card.rank).to eq('Ace')
+    expect(card.rank).to eq('Ace')
   end
 
   it 'has a suit' do 
-    expect(@card.suit).to eq('Spades')
+    expect(card.suit).to eq('Spades')
   end
 end
