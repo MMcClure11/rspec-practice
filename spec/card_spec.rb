9f96153c09b3d85186c9bb884364f430c42d1da8
MMcClure11 <mcclme02@luther.edu>
2021-03-12 09:32:36 -0600

    Refactor test suit to use before block to make the card object

--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -9,13 +9,16 @@ end
 
 RSpec.describe Card do
   #refactor, using the actual Class reference is better than the string because it adds some advanced RSpec helper methods 
+  before do 
+    @card = Card.new('Ace', 'Spades')
+  end
+  #this will be run once before each test
+  
   it 'has a rank' do 
-    card = Card.new('Ace', 'Spades')
-    expect(card.rank).to eq('Ace')
+    expect(@card.rank).to eq('Ace')
   end
 
   it 'has a suit' do 
-    card = Card.new('Ace', 'Spades')
-    expect(card.suit).to eq('Spades')
+    expect(@card.suit).to eq('Spades')
   end
 end
