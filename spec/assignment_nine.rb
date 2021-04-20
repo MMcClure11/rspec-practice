e0f37a4a190eb4ea7e218a8f7ed9a3f725b5d773
MMcClure11 <mcclme02@luther.edu>
2021-04-20 08:31:57 -0500

    My solution to assignment_nine

---
+++ spec/assignment_nine.rb
@@ -0,0 +1,19 @@
+# Questions for this assignment
+# Write a compound expectation that asserts the string firetruck starts with the substring "fire" and ends with the substring "truck".
+RSpec.describe 'firetruck' do 
+  it 'can support a compound expectation' do 
+    expect(subject).to start_with('fire').and end_with('truck')
+  end
+end
+# Write a compound expectation that asserts that the number 20 is even and that it responds to the times mthod.
+RSpec.describe 20 do 
+  it 'can support multiple matchers' do 
+    expect(subject).to be_even.and respond_to(:times)
+  end
+end
+# Write a compound expectation that asserts that the array [4, 8, 15, 16, 23, 42] includes the value 42 and starts with the values 4, 8, and 15.
+RSpec.describe [4, 8, 15, 16, 23, 42] do 
+  it 'can support multiple matchers' do
+    expect(subject).to include(42).and start_with([4, 8, 15])
+  end
+end
